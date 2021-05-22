9ecb1e93a8b22c89e1e30d665002773faf6c8075
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-05-22 17:13:46 -0500

    Added Archive button to list Added an "archive" to each list component and created an archivedItems array in the parent component to keep track of them. Apply double filter.

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
-const List = ({ list }) => {
+const List = ({ list, onArchive }) => {
   return (
     <ul>
       {list.map((item) => (
-        <li key={item.id}> {item.name} </li>
+        <li key={item.id}>
+          {" "}
+          {item.name}
+          <span>
+            <button type="button" onClick={() => onArchive(item.id)}>
+              Archive
+            </button>
+          </span>
+        </li>
       ))}
     </ul>
   );
--- src/Components/SearchableList.jsx
+++ src/Components/SearchableList.jsx
@@ -9,22 +9,53 @@ function byQuery(query) {
 }
 
 const SearchableList = ({ list }) => {
-  const [query, setQuery] = useState({ query: "" });
+  const [query, setQuery] = useState({
+    query: "",
+    filteredList: [],
+    archivedItems: []
+  });
   const onChange = (event) => {
     const { value } = event.target;
-    setQuery({ query: value });
+    setQuery((preVal) => ({
+      ...preVal,
+      query: value
+    }));
   };
 
   const onSubmit = (event) => {
     console.log(query.query);
     event.preventDefault();
   };
+
+  const onArchive = (id) => {
+    let archivedItemsCopy = query.archivedItems.slice();
+    archivedItemsCopy.push(id);
+    console.log(archivedItemsCopy);
+    setQuery((preVal) => ({
+      ...preVal,
+      archivedItems: archivedItemsCopy
+    }));
+    // console.log(query.archivedItems);
+  };
+
+  const byArchived = (archivedItems1) => {
+    return function (item) {
+      console.log("ar", archivedItems1);
+      return !archivedItems1.includes(item.id);
+    };
+  };
+
   return (
     <div>
       <Search query={query.query} onChange={onChange}>
         Search List:{" "}
       </Search>
-      <List list={(list || []).filter(byQuery(query.query))} />
+      <List
+        list={(list || [])
+          .filter(byQuery(query.query))
+          .filter(byArchived(query.archivedItems))}
+        onArchive={onArchive}
+      />
     </div>
   );
 };
