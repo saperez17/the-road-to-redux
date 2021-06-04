1e985f45c01f4ed46998633aa37f293163352e76
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-06-04 10:25:52 -0500

    TodoApp cmpnt converted to a ConnectedTodoApp This approach of using redux with react shows how to convert the root app component to a connected component with redux-react library.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ import TodoApp from "./Components/TodoApp";
 import store from "./store/index";
 import { doAddTodo, doToggleTodo } from "./actions/todoActions";
 
+import { Provider, connect } from "react-redux";
+
+//react-redux. A library to wire up Redux with React.
+//Provider cmpnt: Gives access to the Redux store. Should
+//be at the top level of the app
+
 const App = () => {
   return (
     <Grid>
@@ -22,15 +28,33 @@ const App = () => {
   );
 };
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp
-      todos={store.getState().todoState}
-      onToggleTodo={(id) => store.dispatch(doToggleTodo(id))}
-    />,
-    document.getElementById("container")
-  );
-};
+//This exposes the props to the TodoApp component by using the
+//connect higher-order component. Thus, the TodoApp cmpnt becomes a
+//connectedTodoApp component.
 
-store.subscribe(render);
-render();
+function mapStateToProps(state) {
+  return {
+    todos: state.todoState
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onToggleTodo: (id) => dispatch(doToggleTodo(id))
+  };
+}
+
+const ConnectedTodoApp = connect(mapStateToProps, mapDispatchToProps)(TodoApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedTodoApp
+    // todos={store.getState().todoState}
+    // onToggleTodo={(id) => store.dispatch(doToggleTodo(id))}
+    />
+  </Provider>,
+  document.getElementById("container")
+);
+
+// store.subscribe(render);
+// render();
